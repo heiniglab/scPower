581799387ebba5aef2e4dd72288c7a0e2bbb0ceb
cemgulec <cem.ggulecc@gmail.com>
2023-12-20 21:45:23 +0100

    licence statement added

--- inst/shinyApp/ui.R
+++ inst/shinyApp/ui.R
@@ -53,7 +53,8 @@ sidebar <-   dashboardSidebar(
     menuItem("Welcome", tabName = "welcome", icon = icon("home")),
     menuItem("Description", tabName = "description", icon = icon("book")),
     menuItem("Detect DE/eQTL genes", tabName = "genes", icon = icon("users")),
-    menuItem("Detect cell types", tabName = "celltypes", icon = icon("search"))
+    menuItem("Detect cell types", tabName = "celltypes", icon = icon("search")),
+    menuItem("License Statement", tabName = "license", icon = icon("file"))
   )
 )
 
@@ -375,7 +376,21 @@ body <- ## Body content
                   correctly measured cells (no doublets etc), so the number is a lower bound for the required cells to be sequenced.")
                   )
                 )
-              )
+              ),
+
+      tabItem(tabName = "license",
+        h2("The 3-Clause BSD License"),
+        HTML("
+          <p>Copyright &copy; 2023 Your Company Name</p>
+          <p>Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:</p>
+          <ol>
+            <li>Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.</li>
+            <li>Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.</li>
+            <li>Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.</li>
+          </ol>
+          <p>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</p>
+        ")
+      )
       )
     )
 
